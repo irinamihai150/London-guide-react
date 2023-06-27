9034b477b3b46dacb4cae8e8990c8821fef09045
irinamihai150 <irinamihai150@gmail.com>
2023-06-27 21:40:36 +0100

    fix loading

--- src/components/InfoDetails.jsx
+++ src/components/InfoDetails.jsx
@@ -1,69 +1,80 @@
-import React, { useState, useEffect } from "react";
-import Table from "react-bootstrap/Table";
-import Spinner from "./Spinner";
+import React, { useState, useEffect } from "react"
+import Table from "react-bootstrap/Table"
+import Spinner from "./Spinner"
 
 function InfoDetails({ selectedButton, city }) {
-  const [info, setInfo] = useState([]);
-  const [errorMessage, setErrorMessage] = useState("");
-  const [isLoading, setIsLoading] = useState(true);
+	const [info, setInfo] = useState([])
+	const [errorMessage, setErrorMessage] = useState("")
+	const [isLoading, setIsLoading] = useState(true)
 
-  useEffect(() => {
-    setIsLoading(true);
-    fetch(`https://london-express.onrender.com/${selectedButton}/${city}`)
-      .then((res) => {
-        if (res.ok) {
-          return res.json();
-        } else {
-          throw new Error("Something went wrong");
-        }
-      })
-      .then((info) => setInfo(info))
-      .catch((error) => {
-        setErrorMessage("Unable to fetch data from server");
-      });
-    setIsLoading(false);
-  }, [selectedButton, city]);
-  console.log(info);
-  info.sort(function (a, b) {
-    if (a.name < b.name) {
-      return -1;
-    }
-    if (a.name > b.name) {
-      return 1;
-    }
-    return 0;
-  });
+	useEffect(() => {
+		setIsLoading(true)
+		setInfo([]) // Clear previous data when city changes
+		setErrorMessage("") // Clear previous error message
+		if (city) {
+			fetch(`https://london-express.onrender.com/${selectedButton}/${city}`)
+				.then((res) => {
+					if (res.ok) {
+						return res.json()
+					} else {
+						throw new Error("Something went wrong")
+					}
+				})
+				.then((info) => setInfo(info))
+				.catch((error) => {
+					setErrorMessage("Unable to fetch data from server")
+				})
+				.finally(() => {
+					setIsLoading(false)
+				})
+		} else {
+			setIsLoading(false)
+		}
+	}, [selectedButton, city])
 
-  return isLoading ? (
-    <Spinner />
-  ) : (
-    <div>
-      <Table striped bordered hover responsive className="table">
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Name</th>
-            <th>Website</th>
-            <th>Phone</th>
-            <th>Address</th>
-          </tr>
-        </thead>
-        <tbody>
-          {info.map((i) => {
-            return (
-              <tr key={i.id}>
-                <td>{i.id}</td>
-                <td>{i.name}</td>
-                <td>{i.website}</td>
-                <td>{i.phone}</td>
-                <td>{i.address}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </Table>
-    </div>
-  );
+	info.sort(function (a, b) {
+		if (a.name < b.name) {
+			return -1
+		}
+		if (a.name > b.name) {
+			return 1
+		}
+		return 0
+	})
+
+	return (
+		<div>
+			{isLoading && <Spinner />}
+			{!isLoading && city && (
+				<Table striped bordered hover responsive className='table'>
+					<thead>
+						<tr>
+							<th>#</th>
+							<th>Name</th>
+							<th>Website</th>
+							<th>Phone</th>
+							<th>Address</th>
+						</tr>
+					</thead>
+					<tbody>
+						{info.map((i) => {
+							return (
+								<tr key={i.id}>
+									<td>{i.id}</td>
+									<td>{i.name}</td>
+									<td>{i.website}</td>
+									<td>{i.phone}</td>
+									<td>{i.address}</td>
+								</tr>
+							)
+						})}
+					</tbody>
+				</Table>
+			)}
+			{!isLoading && !city && <p>Please select a city.</p>}
+			{errorMessage && <p>{errorMessage}</p>}
+		</div>
+	)
 }
 
-export default InfoDetails;
+export default InfoDetails

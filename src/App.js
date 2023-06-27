54b5007004a2a167cf8bfc0e2f9a40ab9159530a
irinamihai150 <irinamihai150@gmail.com>
2023-06-27 21:55:18 +0100

    delete search

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
-import Title from "./components/Title";
-import Dropdown from "./components/Dropdown";
-import Button from "./components/Button";
-import Footer from "./components/Footer";
-import React, { useState } from "react";
-import "./App.css";
-import InfoDetails from "./components/InfoDetails";
-import Search from "./components/Search";
+import Title from "./components/Title"
+import Dropdown from "./components/Dropdown"
+import Button from "./components/Button"
+import Footer from "./components/Footer"
+import React, { useState } from "react"
+import "./App.css"
+import InfoDetails from "./components/InfoDetails"
+import Search from "./components/Search"
 
 function App() {
-  const [selectedButton, setSelectedButton] = useState("");
-  const [city, setCity] = useState("");
+	const [selectedButton, setSelectedButton] = useState("")
+	const [city, setCity] = useState("")
 
-
-  return (
-    <div className="App">
-      <Title />
-      <Dropdown setCity={setCity} />
-      <Search/>
-      <Button setSelectedButton={setSelectedButton} />
-      <InfoDetails city={city} selectedButton={selectedButton} />
-      <Footer />
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Title />
+			<Dropdown setCity={setCity} />
+			<Button setSelectedButton={setSelectedButton} />
+			<InfoDetails city={city} selectedButton={selectedButton} />
+			<Footer />
+		</div>
+	)
 }
 
-export default App;
+export default App
